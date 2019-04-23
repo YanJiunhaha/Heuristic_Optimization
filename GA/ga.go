a784f22aaa45438979e4c59eb5643ffe67dbc658
YanJiun <yanjiun@gmail.com>
2019-04-23 14:10:29 +0800

    Init GA

---
+++ GA/ga.go
@@ -0,0 +1,21 @@
+package main
+
+import(
+    "fmt"
+    "time"
+    "math/rand"
+)
+
+const(
+    MG = 50
+    N = 30
+    CL = 32
+    SF = 2.0
+    CR = 0.5
+    MR = 0.05
+)
+
+func main() {
+   fmt.Println("Start.") 
+}
+
